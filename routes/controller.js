22a61c48aec6346106a9fd6edf54e13b2dd289d9
muthukumars1994 <muthukumars0894@gmail.com>
2017-04-12 17:09:45 +0530

    perfect login

--- routes/controller.js
+++ routes/controller.js
@@ -1,5 +1,5 @@
 var bodyParser = require("body-parser");
-var mysql      =   require('mysql');
+var mysql      =   require("mysql");
 var uniqid = require("uniqid");
 var connection =   mysql.createConnection({
   host : 'localhost',
@@ -41,10 +41,10 @@ exports.register = function(req, res){
    //meant for call callback
       var create =
        'insert into users (firstname, lastname, email, mobile, password, access_token) VALUES ("'+req.body.firstname+'", "'+req.body.lastname+'", "'+req.body.email+'", "'+req.body.mobile+'", "'+hash+'","'+uniqid()+'")';
-         console.log(create);
-           connection.query(create, function (err, rows) {
-                  if(err) throw error;
-                      res.json(rows);
+          console.log(create);
+            connection.query(create, function (err, rows) {
+                if(err) throw error;
+                  res.json(rows);
                         //res.redirect('/test');
    });
 };
@@ -55,20 +55,21 @@ exports.login = function(req, res){
     console.log(email);
      var query = 'select * from users where email = "'+email+'"';
         connection.query(query, function(err, rows){
-          console.log(rows);
-          if(err) throw error;
-                var salt  =  bcrypt.genSaltSync(10);
-                var hash  =  bcrypt.hashSync(password, salt);
-                  bcrypt.compare(password, hash, function (err, callback){
-                    console.log(callback);//callback
-                      if(err) throw err;
-                        console.log("mailid==>", email);
-                          console.log(rows[0].firstname);
-                             if(rows[0].email === email && callback){
-                                console.log("logged in");
+          console.log(rows.length);
+            //next(err);
+                 var salt  =  bcrypt.genSaltSync(10);
+                  var hash  =  bcrypt.hashSync(password, salt);
+                    bcrypt.compare(password, hash, function (err, callback){
+                        console.log(callback);//check whether the call back is true or not
+                        //if email is not present [].length===0
+                          if(rows.length != 0){
+                            console.log("mailid==>", email);
+                              if(rows[0].email === email && callback){
+                                 console.log("redirect to login page");
                                 }
-                        else{console.log("not logged in");}
+                            }
+                         else{console.log("redirect to home page");}
                   });
-      res.send(rows);
+            res.send(rows);
        });
 };
